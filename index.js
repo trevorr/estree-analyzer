d54642cc77a0973b3b9d23c884dcd266f35b5e22
Trevor Robinson <trevor@scurrilous.com>
2018-10-09 20:14:36 -0500

    Add format to index

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 'use strict';
 
 const analyze = require('./src/analyze');
+const format = require('./src/format');
 const Scope = require('./src/scope');
 const types = require('./src/types');
 
 module.exports = {
   analyze,
+  format,
   Scope,
   types
 };
